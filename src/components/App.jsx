38febff129bb4ea7399f6dc97dcb1d11735e5075
Halvor William Sanden <halvor.sanden@gmail.com>
2020-03-14 11:27:25 +0100

    Add more values

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -29,6 +29,11 @@ const App = () => {
     fetchCountries()
   }, [])
 
+  function round(value, precision) {
+    const multiplier = Math.pow(10, precision || 0)
+    return Math.round(value * multiplier) / multiplier
+  }
+
   return (
     <>
       <header className="header">
@@ -41,20 +46,30 @@ const App = () => {
               <React.Fragment key={i}>
                 {country.country == 'Norway' ? (
                   <>
-                    {country.country}
+                    <h2>{country.country}</h2>
                     <dl>
-                      <dt>Cases</dt>
+                      <dt>Active cases</dt>
+                      <dd>
+                        {country.cases - country.deaths - country.recovered}
+                      </dd>
+                      <dt>Total cases</dt>
                       <dd>{country.cases}</dd>
                       <dt>New cases today</dt>
                       <dd>{country.todayCases}</dd>
+                      <dt>Increase</dt>
+                      <dd>
+                        {round((country.todayCases / country.cases) * 100, 2)} %
+                      </dd>
+                      <dt>Critical</dt>
+                      <dd>{country.critical}</dd>
                       <dt>Deaths</dt>
                       <dd>{country.deaths}</dd>
                       <dt>New deaths today</dt>
                       <dd>{country.todayDeaths}</dd>
+                      <dt>Deadliness</dt>
+                      <dd>{round(country.deaths / country.cases, 10)} %</dd>
                       <dt>Recovered</dt>
                       <dd>{country.recovered}</dd>
-                      <dt>Critical</dt>
-                      <dd>{country.critical}</dd>
                     </dl>
                   </>
                 ) : null}
