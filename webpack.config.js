fff507da8418b37d209b7329a548ac24d3631813
Halvor William Sanden <william@8yd.no>
2020-03-21 10:47:44 +0100

    Update config and make html template

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: './src/index.jsx',
@@ -22,12 +23,14 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['@babel/preset-env', '@babel/preset-react']
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['@babel/plugin-proposal-class-properties']
         }
       },
       { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
       {
         test: /\.css$/,
+        exclude: /node_modules/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
       }
     ]
@@ -36,6 +39,21 @@ module.exports = {
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'bundle.css'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+      title: 'COVID-19 status',
+      files: {
+        css: 'bundle.css',
+        js: 'bundle.js'
+      },
+      appMountId: 'main',
+      themeColour: '#212121',
+      lang: 'en',
+      minify: true,
+      hash: true,
+      inject: false
     })
   ]
 }
